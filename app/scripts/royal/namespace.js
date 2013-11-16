16e2bb7d6d58b1fa8e7b15e6fe6e150ff7486b98
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2013-11-16 12:33:15 -0600

    Refactored and cleaned up "namespace" library and specs

--- app/scripts/royal/namespace.js
+++ app/scripts/royal/namespace.js
@@ -1,78 +1,79 @@
 (function() 
 {
-	// Bootstrap by defining our createPath method
-	var requirePath = function( path, root ) 
+	var namespace = {};
+	
+	// Executes function on given path
+	namespace.namespace = function( namespacePathStr, func )
 	{
-		// get the different sections of our path
-		var pathParts = path.split(".");
+		var namespacePath = this.requirePath( namespacePathStr );
+		func.call( namespacePath );
+	};
+	
+	// Returns the object at the end of the path. Returns "undefined" if
+	// the path doesn't exist
+	namespace.getPath = function( path )
+	{		
+		var path = this.parsePath( path );
 		
-		// Start trancing the path from the global scope
-		var path = window;
-		for( var index in pathParts )
+		// Travel the path, returning undefined if a node doens't exist
+		var node = this.travelPath( window, path, false );
+		return node;
+	};
+	
+	// Same as "getPath()" but creates nodes that don't exist
+	namespace.requirePath = function( path ) 
+	{
+		var path = this.parsePath( path );
+		
+		// Travel the path, creating nodes that don't exist
+		var node = this.travelPath( window, path, true );
+		return node;
+	};
+	
+	namespace.parsePath = function( path )
+	{
+		if( typeof path == "string" )
 		{
-			var part = pathParts[index];
-			
-			// If path doesn't exist, create it
-			if(path[part] == undefined) 
-			{
-				path[part] = {};
-			}
-			
-			if( typeof path[part] != "object" )
-			{
-				throw { name: "PathCollision" };
-			}
-			
-			// Move down the path
-			path = path[part];
+			path = path.split( "." );
 		}
 		
 		return path;
 	};
 	
-	// Using the new method, we create a path to "namespace" ...
-	var namespace = requirePath( "com.everempire.royal.namespace" );
-	// ... and add the new function to it
-	namespace.requirePath = requirePath;
-	
-	// We can now add the other functions that belong on namespace
-	//////////////////////////////////////////////////////////////
-	
-	// TODO: [prince] find some way to refactor requirePath() and getPath()
-	// to avoid code duplication
-	
-	// Same as requirePath() but returns 
-	// undefined if the path doens't exist
-	namespace.getPath = function( path )
-	{		
-		// get the different sections of our path
-		var pathParts = path.split(".");
-		
-		// Start trancing the path from the global scope
-		var path = window;
-		for( var index in pathParts )
+	namespace.travelPath = function( root, path, force )
+	{
+		var node = root;
+		if( node == undefined )
 		{
-			var part = pathParts[index];
+			node = window;
+		}
+		
+		for( var index in path )
+		{
+			var part = path[index];
 			
-			// If path doesn't exist, return undefined
-			if(path[part] == undefined) 
+			if(node[part] == undefined) 
 			{
-				return undefined;
+				if( !force )
+				{
+					return undefined;
+				}
+				
+				if( typeof node != "object" )
+				{
+					throw { name: "PathCollision" };
+				}
+				else
+				{
+					node[part] = {};
+				}
 			}
 			
 			// Move down the path
-			path = path[part];
+			node = node[part];
 		}
 		
-		return path;
-	}
-	
-	// TODO [prince] implement this properly, WRITE A TEST FOR IT FIRST
-	// Adds function to 
-	namespace.namespace = function( namespacePathStr, func )
-	{
-		var namespacePath = this.requirePath( namespacePathStr );
-		func.call( namespacePath );
+		return node;
 	};
 	
 	// Export to global scope
--- test/unit/royal/namespaceSpec.js
+++ test/unit/royal/namespaceSpec.js
@@ -1,90 +1,97 @@
 describe( "namespace", function() 
 {
+	beforeEach( function() 
+	{
+		// Build test path manually
+		window["a"] = {};
+		window["a"]["path"] = {};
+		window["a"]["path"]["that"] = {};
+		var that = window["a"]["path"]["that"];
+		
+		that["exists"] = {};
+		
+		that["ends"] = {};
+		that["ends"]["with"] = {};
+		that["ends"]["with"]["a"] = {};
+		that["ends"]["with"]["a"]["string"] = "The end of the path";
+	});
+	
+	afterEach( function() 
+	{
+		// Clean up "window"
+		delete window["a"];
+	});
+	
 	describe( "requirePath()", function()
 	{
-		beforeEach( function() 
-		{
-			// Build test path
-			window["this"] = {};
-			window["this"]["path"] = {};
-			window["this"]["path"]["exists"] = {};
-		});
-		
-		afterEach( function() 
-		{
-			// Clean up created paths
-			delete window["this"];
-		});
-		
 		it( "gets a path based upon a namespace string", function() 
 		{
-			var path = namespace.requirePath( "this.path.doesnt.exist.yet.but.it.will" )
+			var path = namespace.requirePath( "a.path.that.doesnt.exist.yet.but.it.will" )
 			expect( path ).toEqual( {} );
 		});
 		
 		it( "creates a path if it doesn't exist", 
 		function() 
 		{
-			var node = namespace.requirePath( "this.path.doesnt.exist.yet.but.it.will" );
+			var node = namespace.requirePath( "a.path.that.doesnt.exist.yet.but.it.will" );
 			expect( node ).toEqual( {} );
 		});
 		
 		it( "throws a \"PathCollision\" error when a non-object is already on the path", 
 		function() 
 		{
-			var firstNode = namespace.requirePath( "this.path.ends.with.a" );
-			firstNode.string = "This is the end of the path";
+			var callRequirePath = function() { 
+				namespace.requirePath( "a.path.that.ends.with.a.string.i.think" ) 
+			}
 			
-			expect( function() { 
-				namespace.requirePath( "this.path.ends.with.a.string.i.think" ) 
-			} ).toThrow( { name: "PathCollision" } );
+			expect( callRequirePath ).toThrow( { name: "PathCollision" } );
 		});
 	});
 	
 	describe( "getPath()", function()
 	{
-		afterEach( function() 
-		{
-			// Clean up created paths
-			delete window["this"];
-		});
-		
 		it( "gets a path based upon a namespace string", function() 
 		{
-			// Create path
-			var testNode = namespace.requirePath( "this.is.a.test.path" );
-			
-			// GetPath
-			var node = namespace.getPath( "this.is.a.test.path" );
-			expect( node ).toBe( testNode );
+			var node = namespace.getPath( "a.path.that.exists" );
+			expect( node ).toEqual( {} );
 		});
 		
-		it( "returns undefined if the path doesn't exist", function() 
+		it( "returns \"undefined\" if the path doesn't exist", function() 
 		{
-			// GetPath
-			var node = namespace.getPath( "this.path.doesnt.exist" );
+			var node = namespace.getPath( "a.path.that.doesnt.exist" );
+			expect( node ).toBe( undefined );
+		});
+		
+		it( "returns \"undefined\" when a non-object is already on the path", 
+		function() 
+		{
+			var node = namespace.getPath( "a.path.that.ends.with.a.string.i.think" ) 
 			expect( node ).toBe( undefined );
 		});
 	});
 	
 	describe( "namespace()", function()
 	{
-		afterEach( function() 
-		{
-			// Clean up created paths
-			delete window["this"];
-		});
-		
 		it( "calls a function within the scope of the given path", function() 
 		{
 			var myName = "Edward";
-			namespace.namespace( "this.is.a.test.namespace", function() 
+			namespace.namespace( "a.test.path", function() 
 			{
 				this.name = myName;
 			});
 			
-			var node = namespace.getPath( "this.is.a.test.namespace" );
+			var node = namespace.getPath( "a.test.path" );
 			expect( node.name ).toBe( myName );
+		});
+	});
+	
+	describe( "parsePath()", function()
+	{
+		it( "converts a string to a path array", function() 
+		{
+			var path = namespace.parsePath( "a.test.path" );
+			
+			expect( path ).toEqual( [ "a", "test", "path" ] );
 		});
 	});
 });
