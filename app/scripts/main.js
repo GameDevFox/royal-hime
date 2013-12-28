d63bd5cc7ffb9114fc0e8d2717eb2b4dec6e9aa7
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2013-12-27 22:36:25 -0600

    Minor clean up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,41 +1,24 @@
 (function()
-{
-	var areaDefObject = null;
-	var actorDefObject = null;
-	
+{	
 	var resources = 
 	{
 		areaData: "/data/areas.json",
 		actorData: "/data/actors.json"
 	};
-	
 	var loadedResources = {};
 	
 	var onReady = function()
 	{
-		each( resources, function( value, key )
+		each( resources, loadAndCheckResource );
+	};
+	
+	var loadAndCheckResource = function( value, key )
+	{
+		$.get( value, function( data )
 		{
-			$.get( value, function( data )
-			{
-				loadedResources[key] = data;
-				checkLoaded( loadedResources );
-			});
+			loadedResources[key] = data;
+			checkLoaded( loadedResources );
 		});
-		
-		// TODO: [prince] Create download manager
-//		var areaDataPath = "/data/areas.json";
-//		$.get( areaDataPath, function( data )
-//		{
-//			areaDefObject = data;
-//			checkLoaded();
-//		});
-//		
-//		var actorDataPath = "/data/actors.json";
-//		$.get( actorDataPath, function( data )
-//		{
-//			actorDefObject = data;
-//			checkLoaded();
-//		});
 	};
 	
 	var checkLoaded = function( loadedResources ) 
@@ -59,16 +42,14 @@
 		var injectModuleData = function( value, key )
 		{
 			himeModule.constant( key, value );
-		}
+		};
 		each( loadedResources, injectModuleData );
 		
 		//Bootstrap angularjs
-		angular.bootstrap( $("body"), [ "Hime", "Time" ] );
+		window.injector = angular.bootstrap( $("body"), [ "Hime", "Time" ] );
 		
 		window.rootScope = getScope( "body" );
 	};
-	
-	
 	
 	$(document).ready( onReady );
 }());
