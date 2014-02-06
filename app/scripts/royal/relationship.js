ed73abc43f304f549c44e982b78906d5dbf3728d
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-02-06 12:13:03 -0600

    Added "data" binding to relationship system

--- app/scripts/royal/relationship.js
+++ app/scripts/royal/relationship.js
@@ -1,91 +1,114 @@
 (function()
 {
-	this.hasBothNodes = function( relation, nodeA, nodeB )
-	{
-		var hasBothNodes = 
-			( relation[0] == nodeA && relation[1] == nodeB ) ||
-			( relation[0] == nodeB && relation[1] == nodeA );
-		return hasBothNodes;
-	};
-	
-	this.getRelation = function( relations, nodeA, nodeB )
-	{
-		var relation = _.find( relations, function( relation )
-		{
-			var hasBothNodes = this.hasBothNodes( relation, nodeA, nodeB );
-			return hasBothNodes;
-		});
-		
-		return relation;
-	};
-	
 	namespace.namespace("com.everempire.royal.relationship", function()
 	{
+		var hasNode = function( nodes, node )
+		{
+			var result = nodes[0] == node || nodes[1] == node;
+			return result;
+		};
+
+		var hasBothNodes = function( nodes, nodeA, nodeB )
+		{
+			var result =
+				( nodes[0] == nodeA && nodes[1] == nodeB ) ||
+				( nodes[0] == nodeB && nodes[1] == nodeA );
+			return result;
+		};
+
+		var getRelation = function( relations, nodeA, nodeB )
+		{
+			var relation = _.find( relations, function( relation )
+			{
+				var result = hasBothNodes( relation.nodes, nodeA, nodeB );
+				return result;
+			});
+
+			return relation;
+		};
+
 		this.createRelationshipSystem = function()
 		{
 			var relationshipSystem = {};
-			
+
 			relationshipSystem.relations = [];
-			
-			relationshipSystem.addRelationship = function( nodeA, nodeB )
+
+			relationshipSystem.createRelationship = function( nodeA, nodeB )
 			{
 				if( relationshipSystem.hasRelationship( nodeA, nodeB ) )
 				{
-					throw { 
-						msg: "There is already a relationship between these two nodes", 
+					throw {
+						msg: "There is already a relationship between these two nodes",
 						nodeA: nodeA,
 						nodeB: nodeB
 					};
 				}
-				
-				var relation = [ nodeA, nodeB ];
+
+				var data = {};
+				var relation = { nodes: [ nodeA, nodeB ], data: data };
 				relationshipSystem.relations.push( relation );
+
+				return data;
 			};
-			
+
+			relationshipSystem.getRelationship = function( nodeA, nodeB )
+			{
+				if( !relationshipSystem.hasRelationship( nodeA, nodeB ) )
+				{
+					return null;
+				}
+
+				var relation = getRelation( relationshipSystem.relations, nodeA, nodeB );
+				var data = relation.data;
+
+				return data;
+			};
+
 			relationshipSystem.removeRelationship = function( nodeA, nodeB )
 			{
 				if( !relationshipSystem.hasRelationship( nodeA, nodeB ) )
 				{
-					throw { 
-						msg: "There is no relationship between these two nodes", 
+					throw {
+						msg: "There is no relationship between these two nodes",
 						nodeA: nodeA,
 						nodeB: nodeB
 					};
 				}
-				
+
 				relationshipSystem.relations = _.reject( relationshipSystem.relations, function( relation )
 				{
-					var hasBothNodes = this.hasBothNodes( relation, nodeA, nodeB );
-					return hasBothNodes;
+					var result = hasBothNodes( relation.nodes, nodeA, nodeB );
+					return result;
 				});
 			};
-			
+
 			relationshipSystem.hasRelationship = function( nodeA, nodeB )
 			{
 				var relation = getRelation( relationshipSystem.relations, nodeA, nodeB );
 				var hasRelationship = ( relation != undefined );
-				
+
 				return hasRelationship;
 			};
-			
+
 			relationshipSystem.getRelatedNodes = function( node )
 			{
+				// Get all relations that have this node
 				var filteredRelations = _.filter( relationshipSystem.relations, function( relation )
 				{
-					var hasNode = relation[0] == node || relation[1] == node;
-					return hasNode;
+					var result = hasNode( relation.nodes, node);
+					return result;
 				});
-				
+
 				var relations = _.map( filteredRelations, function( relation )
 				{
-					var remainingNodeArray = _.without( relation, node );
+					var remainingNodeArray = _.without( relation.nodes, node );
 					var remainingNode = remainingNodeArray[0];
 					return remainingNode;
 				});
-				
+
 				return relations;
 			};
-			
+
 			return relationshipSystem;
 		};
 	});
--- test/unit/royal/relationship-spec.js
+++ test/unit/royal/relationship-spec.js
@@ -9,114 +9,148 @@ describe( "com.everempire.royal.relationship", function()
 			var relationshipSystem = $relationship.createRelationshipSystem();
 			expect( relationshipSystem ).not.toEqual( null );
 		});
-		
+
 		describe("RelationshipSystem", function()
 		{
 			var relationshipSystem;
-			
+
 			var nodeA = { name: "nodeA" };
 			var nodeB = { name: "nodeB" };
 			var nodeC = { name: "nodeC" };
 			var nodeD = { name: "nodeD" };
-			
+
 			beforeEach( function()
 			{
 				relationshipSystem = $relationship.createRelationshipSystem();
 			});
-			
-			describe("addRelationship( nodeA, nodeB )", function()
+
+			describe("createRelationship( nodeA, nodeB )", function()
 			{
+				var data;
+
 				beforeEach( function()
 				{
-					relationshipSystem.addRelationship( nodeA, nodeB );
+					data = relationshipSystem.createRelationship( nodeA, nodeB );
 				});
-				
+
+				it("returns a relationship data object", function()
+				{
+					expect(data).toEqual({});
+				});
+
 				it("creates a relationship between two nodes", function()
 				{
 					var relatedNodes = relationshipSystem.getRelatedNodes( nodeA );
 					expect( relatedNodes ).toEqual( [ nodeB ] );
-					
+
 					relatedNodes = relationshipSystem.getRelatedNodes( nodeB );
 					expect( relatedNodes ).toEqual( [ nodeA ] );
 				});
-				
+
 				it("throws exception if a relation between the two nodes already exists", function()
 				{
-					var addRelationshipFunc = function() 
-					{ 
-						relationshipSystem.addRelationship( nodeB, nodeA ); 
+					var createRelationshipFunc = function()
+					{
+						relationshipSystem.createRelationship( nodeB, nodeA );
 					};
-					
-					expect( addRelationshipFunc )
-						.toThrow( { 
-							msg: "There is already a relationship between these two nodes", 
+
+					expect( createRelationshipFunc )
+						.toThrow( {
+							msg: "There is already a relationship between these two nodes",
 							nodeA: nodeB,
 							nodeB: nodeA
 						});
 				});
 			});
-			
+
+			describe("getRelationship(nodeA, nodeB)", function()
+			{
+				beforeEach( function()
+				{
+					data = relationshipSystem.createRelationship( nodeA, nodeB );
+					data.name = "MyData";
+				});
+
+				it("returns the data object indexed by these two nodes", function()
+				{
+					var data = relationshipSystem.getRelationship( nodeA, nodeB );
+					expect(data).toEqual({ name: "MyData" });
+				});
+
+				it("is associative (order doesn't matter)", function()
+				{
+					var data = relationshipSystem.getRelationship( nodeB, nodeA );
+					expect(data).toEqual({ name: "MyData" });
+				});
+
+				it("returns null if relationship doesn't exist", function()
+				{
+					var data = relationshipSystem.getRelationship( nodeA, nodeC );
+					expect(data).toEqual(null);
+				});
+			});
+
 			describe("removeRelationship( nodeA, nodeB )", function()
 			{
 				it("removes a relationship between two nodes", function()
 				{
-					relationshipSystem.addRelationship( nodeA, nodeB );
-					relationshipSystem.addRelationship( nodeA, nodeC );
-					
+					relationshipSystem.createRelationship( nodeA, nodeB );
+					relationshipSystem.createRelationship( nodeA, nodeC );
+
 					relationshipSystem.removeRelationship( nodeA, nodeB );
-					
+
 					var relatedNodes = relationshipSystem.getRelatedNodes( nodeA );
-					
+
 					expect( relatedNodes ).toEqual( [ nodeC ] );
 				});
-				
+
 				it("throws exception if a relation between the two nodes does not exist", function()
 				{
-					var removeRelationshipFunc = function() 
-					{ 
-						relationshipSystem.removeRelationship( nodeB, nodeA ); 
+					var removeRelationshipFunc = function()
+					{
+						relationshipSystem.removeRelationship( nodeB, nodeA );
 					};
-					
+
 					expect( removeRelationshipFunc )
-						.toThrow( { 
-							msg: "There is no relationship between these two nodes", 
+						.toThrow( {
+							msg: "There is no relationship between these two nodes",
 							nodeA: nodeB,
 							nodeB: nodeA
 						});
 				});
 			});
-			
+
 			describe("hasRelationship( nodeA, nodeB )", function()
 			{
-				beforeEach( function() 
+				beforeEach( function()
 				{
-					relationshipSystem.addRelationship( nodeA, nodeB );
+					relationshipSystem.createRelationship( nodeA, nodeB );
 				});
-				
+
 				it("returns true if this system contains a relationship between the two nodes", function()
 				{
 					expect( relationshipSystem.hasRelationship( nodeA, nodeB ) ).toEqual( true );
 				});
-				
+
 				it("returns false if this system does not contain a relationship between the two nodes", function()
 				{
 					expect( relationshipSystem.hasRelationship( nodeA, nodeC ) ).toEqual( false );
 				});
 			});
-			
+
 			describe("getRelatedNodes( node )", function()
 			{
 				it("returns a list of nodes that are related to the given node", function()
 				{
 					var expectedRelations = [ nodeB, nodeD ];
-					
-					relationshipSystem.addRelationship( nodeA, nodeB );
-					relationshipSystem.addRelationship( nodeB, nodeD );
-					relationshipSystem.addRelationship( nodeB, nodeC );
-					relationshipSystem.addRelationship( nodeD, nodeA );
-					
+
+					relationshipSystem.createRelationship( nodeA, nodeB );
+					relationshipSystem.createRelationship( nodeB, nodeD );
+					relationshipSystem.createRelationship( nodeB, nodeC );
+					relationshipSystem.createRelationship( nodeD, nodeA );
+
 					var relatedNodes = relationshipSystem.getRelatedNodes( nodeA );
-					
+
 					expect( _.difference( expectedRelations, relatedNodes ) ).toEqual( [] );
 				});
 			});
