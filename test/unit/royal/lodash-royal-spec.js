34be4c1d61ab7789bbc155734f1dc6b9fc80a8e9
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-03-28 23:38:06 -0500

    Added unit test for _.zipObjects

--- test/unit/royal/lodash-royal-spec.js
+++ test/unit/royal/lodash-royal-spec.js
@@ -21,4 +21,25 @@ describe( "lodash-royal.js mixins", function()
 			expect(result).toEqual({ firstOne: 9, secondOne: 5, lastOne: 9 });
 		});
 	});
+
+	describe("_.mapByName(collection, keyIterator, valueIterator, context)", function()
+	{
+		it("behaves like _.zipObject but accepts and returns multiple values", function()
+		{
+			var objects =_.zipObjects(
+					// Keys
+					["name", "age", "gender"],
+					// Values
+					["James", 24, "m"],
+					["Sue", 21, "f"],
+					["John", 32, "m"]
+			);
+
+			expect(objects).toEqual([
+				{ "name": "James", "age": 24, "gender": "m" },
+				{ "name": "Sue", "age": 21, "gender": "f" },
+				{ "name": "John", "age": 32, "gender": "m" }
+			]);
+		});
+	});
 });
