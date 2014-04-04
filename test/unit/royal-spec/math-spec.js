5c7cbbaf2ebf4d97fa33b6d06bc0da5f8de2f2b8
Edward Nicholes Jr <prince@everempire.com>
2014-04-04 10:54:25 -0500

    Full unit test coverage for "royal/math"

--- test/unit/royal-spec/math-spec.js
+++ test/unit/royal-spec/math-spec.js
@@ -2,12 +2,37 @@ define(["royal/math"], function($math)
 {
 	describe( "royal/math", function()
 	{
-		it( "returns theta as-is", function()
+		describe("linear(theta)", function()
 		{
-			var theta = 0.123;
+			it( "returns theta as-is", function()
+			{
+				var theta = 0.123;
 
-			var out = $math.filters.linear( theta );
-			expect( out ).toEqual( theta );
+				var out = $math.filters.linear(theta);
+				expect(out).toEqual(theta);
+			});
+		});
+
+		describe("easeInCubic(theta)", function()
+		{
+			it( "returns an \"easeIn\" curve via theta", function()
+			{
+				var theta = 0.5;
+
+				var out = $math.filters.easeInCubic(theta);
+				expect(out).toEqual(0.875);
+			});
+		});
+
+		describe("easeOutCubic(theta)", function()
+		{
+			it( "returns an \"easeOut\" curve via theta", function()
+			{
+				var theta = 0.5;
+
+				var out = $math.filters.easeOutCubic(theta);
+				expect(out).toEqual(0.125);
+			});
 		});
 	});
 });
