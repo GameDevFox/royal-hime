5c31aea1badd668f7d4d64ab67b498ed43600fff
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-06-28 17:42:34 -0500

    Cleaned up "bin" and "test" folder

--- bin/testing-server-start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-java -jar test/jscover/JSCover-all.jar -ws --report-dir=test/jscover/report \
-	--no-instrument=app/scripts/lib \
-	--no-instrument=test/jasmine \
-	--no-instrument=test/unit
